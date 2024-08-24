3afd8877e8ffe0eb8d4690ea6f65248388b4f1c6
OIITuMuCT <vataman@live.ru>
2024-08-24 20:13:16 +0300

    Добавленны функции представления на основе классов ListView, DetailView и тесты

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,28 @@
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponseRedirect
+from .models import Choice, Question
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        """Return the last five published questions.(not including those set to be
+    published in the future)."""
+        return Question.objects.filter(pub_date__lte_=timezone.now()).order_by("-pub_date")[:5]
+    
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def index(request):
@@ -9,13 +30,37 @@ def index(request):
     context = {"latest_question_list": latest_question_list,}
     return render(request, 'polls/index.html', context)
 
+
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
-    response = "You're looking at the af question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST - объект, похожий на словарь, который 
+        # позволяет получать доступ к отправленным данным по 
+        # имени ключа. Значения request.POST['Имя ключа'] всегда строка.
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message":"You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Всегда возвращайте HttPResponseRedirect после успешной обработки 
+        # данных методом POST. Это предотвращает повторную публикацию данных, 
+        # если пользователь нажимает кнопку назад.
+        return HttpResponseRedirect(reverse("polls:results", args=(question_id)))
