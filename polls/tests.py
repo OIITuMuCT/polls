2937a0f7a95cde199082bc96d83a2bbb6e4a03ed
OIITuMuCT <vataman@live.ru>
2024-08-24 20:26:36 +0300

    Добавлена функция test_past_question

--- polls/tests.py
+++ polls/tests.py
@@ -25,12 +25,18 @@ class QuestionIndexViewTests(TestCase):
         self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
 
-        def test_past_question(self):
-            """
-            Questions with a pub_date in the past are displayed on the 
-            index page.
-            """
-            pass
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the 
+        index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+                response.context["latest_question_list"],
+                [question],
+            )
 
 
 class QuestionModelTest(TestCase):
