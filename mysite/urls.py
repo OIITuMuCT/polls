c15543996d9bfaadfebde1435fa8aa553bd9f661
OIITuMuCT <vataman@live.ru>
2024-08-30 15:13:31 +0300

    add base and login.html and base.css

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 
 
 urlpatterns = [
+    path('account/', include("account.urls")),
     path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
 ]
