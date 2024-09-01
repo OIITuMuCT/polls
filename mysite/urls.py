aa62ce68bd1f7e7f3d26951b33523f8b521d6872
OIITuMuCT <vataman@live.ru>
2024-09-01 18:59:25 +0300

    migration db

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,16 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('account/', include("account.urls")),
     path('polls/', include("polls.urls")),
-    path('admin/', admin.site.urls),
-    # path('')
 ]
 
 if settings.DEBUG:
     urlpatterns.append(
         path('__debug__/', include("debug_toolbar.urls"))
+    )
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
     )
