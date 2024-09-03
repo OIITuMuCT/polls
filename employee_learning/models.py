aa511c4b91c448367600a3e2e514b4d212a41b24
OIITuMuCT <vataman@live.ru>
2024-09-03 03:41:16 +0300

    add priorities choices

--- employee_learning/models.py
+++ employee_learning/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    name = models.CharField(max_length=25)
+    PRIORITIES = [('H', 'High'), ('M', 'Medium'), ('L', 'Low'),]
+    name = models.CharField(max_length=25, verbose_name="Employee Name")
+    priority = models.CharField(max_length=1, 
+                                verbose_name="Learning Priorities", 
+                                choices=PRIORITIES, default='M')
 
     def __str__(self):
-        return f'{self.id} : {self.name}'
+        return self.name
 
 class Division(models.Model):
     div_name = models.CharField(max_length=25)
