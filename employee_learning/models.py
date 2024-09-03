fda2f650d7f480d83c54bd7e870ebaf0f7d4eb08
OIITuMuCT <vataman@live.ru>
2024-09-03 03:15:49 +0300

    before migrations employee

--- employee_learning/models.py
+++ employee_learning/models.py
@@ -3,7 +3,15 @@ from django.db import models
 # Create your models here.
 class Employee(models.Model):
     name = models.CharField(max_length=25)
-    
+
+    def __str__(self):
+        return f'{self.id} : {self.name}'
 
 class Division(models.Model):
     div_name = models.CharField(max_length=25)
+    in_scope = models.BooleanField(
+        help_text="If the division is target for the learning program, check this field.")
+
+
+    def __str__(self):
+        return self.div_name
