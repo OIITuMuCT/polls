4fae07f6a8228fff7e538c5131ebd9e4849d0909
OIITuMuCT <vataman@live.ru>
2024-09-04 00:17:36 +0300

    разметка страницы

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [],
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [],
+        # 'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
